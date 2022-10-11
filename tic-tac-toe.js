1173ab7557981bd80aedc16eb817e3dbc99a0565
jhubbell89 <ewhs4jamesh@hotmail.com>
2022-10-10 20:55:56 -0700

    made wining mesage and restart button in html. defined containersin js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,70 @@
 //Define all the containers
+window.onload = function () {
+    var el = document.getElementById('div1');
+    el.onclick = sq1;
+}
+window.onload = function () {
+    var el = document.getElementById('div2');
+    el.onclick = sq2;
+}
+// window.onload = function () {
+//     var el = document.getElementById('div3');
+//     el.onclick = sq3;
+// }
+// window.onload = function () {
+//     var el = document.getElementById('div4');
+//     el.onclick = sq4;
+// }
+// window.onload = function () {
+//     var el = document.getElementById('div5');
+//     el.onclick = sq5;
+// }
+// window.onload = function () {
+//     var el = document.getElementById('div6');
+//     el.onclick = sq6;
+// }
+// window.onload = function () {
+//     var el = document.getElementById('div7');
+//     el.onclick = sq7;
+// }
+// window.onload = function () {
+//     var el = document.getElementById('div8');
+//     el.onclick = sq8;
+// }
+// window.onload = function () {
+//     var el = document.getElementById('div9');
+//     el.onclick = sq9;
+// }
+
+//for testing purposes
+function sq2() {
+    console.log('hellos');
+}
+//Reset game control
+
+//define player
+
+//Choose X or O for player
+
+
+//X object
+// const x = 'X'
+
+// //O object
+// const o = 'O'
+
+// //display whos turn it is
+
+
+//win logic and winning message
+
+
+
+//clicking squares
+// function sq1() {
+//     if (player === 1) {
+//         'div1' = 'x'
+//     }
+//     } else {
+//         'div1' = 'o'
+//     }
