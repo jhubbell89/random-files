bdc964dbb816fc1cad8bc8cf5d126b32797b611b
jhubbell89 <ewhs4jamesh@hotmail.com>
2022-10-12 15:36:26 -0700

    worked on js defined const for submit btn and started event listener

---
+++ mydevskillslab/js_main.js
@@ -0,0 +1,13 @@
+
+// submit button
+const addSkillsBtn = document.getElementById('addskillbtn')
+var inputBox = document.getElementById('skillinput')
+
+
+//addEventListener
+const listenToBtn = document.querySelector()
+
+const myForm = document.querySelector('');
+myForm.addEventListener("submit", function(evt) {
+    evt.preventDefault ();
+});
